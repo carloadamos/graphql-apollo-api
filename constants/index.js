d5f0b2278c4e68774df9cab3843be72a88b41b77
carloadamos <johncarloadamos@gmail.com>
2021-03-12 15:56:09 +0800

    Modify sample data

--- constants/index.js
+++ constants/index.js
@@ -7,7 +7,7 @@ module.exports.tasks = [
 
 module.exports.users = [
   {id: "1", name: "kevin", email: "kevin@gmail.com"},
-  {id: "1", name: "john", email: "john@gmail.com"},
-  {id: "1", name: "peter", email: "peter@gmail.com"},
-  {id: "1", name: "bob", email: "bob@gmail.com"},
+  {id: "2", name: "john", email: "john@gmail.com"},
+  {id: "3", name: "peter", email: "peter@gmail.com"},
+  {id: "4", name: "bob", email: "bob@gmail.com"},
 ];
